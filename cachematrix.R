5eb68afc1159fc81f5c37c289d0dc86439fcb5b4
ramanraja <rajaraman.v@gmail.com>
2014-04-23 23:23:16 +0530

    Functions for caching a matrix inverse
    Wrote the functions for inverting a non-singular matrix and caching the
    inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# This is a matrix object with functions for getting/setting the 
+# object and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        # getter: returns the object
+        get <- function() x
+        # setter: sets the inverse
+        setinverse <- function(inv) m <<- inv
+        # returns the inverse object 
+        getinverse <- function() m
+        # returns a list of 4 functions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# Find the inverse of a square matrix x and cache it
+# argument x must be an object returned by makeCacheMatrix()
+# assumption: x is non-singular
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached inverse")
+                return(m)  # return the cached inverse (after the first invokation)
+        }
+        data <- x$get()
+        m <- solve(data, ...)  # compute the inverse of the matrix (on first invocation)
+        x$setinverse(m)  # store it for futuer use
+        m  # return the inverse
 }
+
+# a quick test
+set.seed(1)
+x = matrix(rnorm(16,10,2), 4,4)
+x
+solve(x) # to ensure it is not singular
+# test the functions
+mvlist = makeCacheMatrix(x)
+str(mvlist)
+mvlist$set(x)
+cacheSolve(mvlist)  # compute and cache the inverse
+cacheSolve(mvlist)  # return inverse from cache
+
